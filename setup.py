69cbf900ea9b17257641e17602e618dbc602f8ee
artem_baskakov <ab@exante.eu>
2018-03-27 19:42:23 +0300

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'https://github.com/wannabegeek/PyFIX/',
     'download_url': 'https://github.com/wannabegeek/PyFIX/',
     'author_email': 'tom@wanabegeek.com.',
-    'version': '0.1',
+    'version': '0.1.1',
     'install_requires': [''],
     'packages': ['pyfix', 'pyfix/FIX44'],
     'scripts': [],
